5bede6db35d3ab419893e848b4300e2e9b75f24d
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2010-12-28 18:14:21 +0100

    [Samples] The (CSV) sample files are now part of the data installed/exported by the StdAir project.

---
+++ samples/sources.mk
@@ -0,0 +1,8 @@
+sample_csv_sources = \
+	$(top_srcdir)/samples/schedule01.csv \
+	$(top_srcdir)/samples/schedule02.csv \
+	$(top_srcdir)/samples/demand01.csv \
+	$(top_srcdir)/samples/rm01.csv \
+	$(top_srcdir)/samples/rm02.csv \
+	$(top_srcdir)/samples/rm03.csv \
+	$(top_srcdir)/samples/rm04.csv
