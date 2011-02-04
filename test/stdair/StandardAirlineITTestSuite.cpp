13a22fc98284009ef61e30f18e5073477de88735
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-02-04 16:59:16 +0100

    [Test] The </TestLog> end tag is now appended to the XML result of the Boost.UTF tests.

--- test/stdair/StandardAirlineITTestSuite.cpp
+++ test/stdair/StandardAirlineITTestSuite.cpp
@@ -38,7 +38,8 @@ namespace boost_utf = boost::unit_test;
  */
 struct UnitTestConfig {
   /** Constructor. */
-  UnitTestConfig() : _test_log ("StandardAirlineITTestSuite_utfresults.xml")  {
+  UnitTestConfig() {
+	static std::ofstream _test_log ("StandardAirlineITTestSuite_utfresults.xml");
     boost_utf::unit_test_log.set_stream (_test_log);
     boost_utf::unit_test_log.set_format (boost_utf::XML);
     boost_utf::unit_test_log.set_threshold_level (boost_utf::log_test_units);
@@ -47,10 +48,7 @@ struct UnitTestConfig {
 
   /** Destructor. */
   ~UnitTestConfig() {
-    boost_utf::unit_test_log.set_stream (std::cout);
   }
-  /** Log file */  
-  std::ofstream _test_log;
 };
 
 
