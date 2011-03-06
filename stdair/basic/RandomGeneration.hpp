c6e050d085f442b296022e9dba4fc65911637a2f
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-03-06 23:06:11 +0100

    [Dev] Specified a little bit more the uniform generator, from uniform_real<> to uniform_01<>.

--- stdair/basic/RandomGeneration.hpp
+++ stdair/basic/RandomGeneration.hpp
@@ -10,55 +10,105 @@
 // StdAir
 #include <stdair/stdair_basic_types.hpp>
 #include <stdair/stdair_maths_types.hpp>
+#include <stdair/basic/StructAbstract.hpp>
 
 namespace stdair {
 
-  /** Random generator. */
-  struct RandomGeneration {
+  /**
+   * @brief Class holding a random generator.
+   */
+  struct RandomGeneration : public StructAbstract {
+  public:
     // //////////// Business Methods /////////////
-    /** Generate a randomized number following a uniform distribution
-        between 0 (included) and 1 (excluded). */
-    RealNumber_T generateUniform01 ();
+    /**
+     * Generate a randomised number following a uniform distribution
+     * between 0 (included) and 1 (excluded).
+     */
+    RealNumber_T generateUniform01();
+
+    /**
+     * Same as generateUniform01(). That operator is provided for
+     * convenient reasons.
+     */
+    RealNumber_T operator()() {
+      return generateUniform01();
+    }
     
-    /** Generate a randomized number following a uniform distribution
-        between a minimum (included) and a maximum (excluded)
-        value. */
+    /**
+     * Generate a randomized number following a uniform distribution
+     * between a minimum (included) and a maximum (excluded)
+     * value.
+     */
     RealNumber_T generateUniform (const RealNumber_T&, const RealNumber_T&);
     
-    /** Generate a randomized number following a normal distribution
-        specified by a mean and a standard deviation. */
+    /**
+     * Generate a randomized number following a normal distribution
+     * specified by a mean and a standard deviation.
+     */
     RealNumber_T generateNormal (const RealNumber_T&, const RealNumber_T&);
     
+
+  public:
+    // ////////////// Display Support Methods //////////
+    /**
+     * Give a description of the structure (for display purposes).
+     */
+    const std::string describe() const;
+
+
+  public:
     // ////////// Constructors and destructors /////////
-    /** Constructor. */
+    /**
+     * Main constructor.
+     */
     RandomGeneration (const RandomSeed_T&);
-    /** Default constructors. */
-    RandomGeneration ();
+    /**
+     * Default constructor.
+     */
+    RandomGeneration();
+    /**
+     * Copy constructor.
+     */
     RandomGeneration (const RandomGeneration&);
-    /** Destructor. */
-    ~RandomGeneration ();
-    /** Initialize the random generator.
-        <br>A uniform random number distribution is defined, which
-        produces "real" values between 0 and 1 (0 inclusive, 1
-        exclusive). */
-    void init ();
+    /**
+     * Copy operator.
+     */
+    RandomGeneration& operator= (const RandomGeneration& iRandomGeneration) {
+      _seed = iRandomGeneration._seed;
+      _generator = iRandomGeneration._generator;
+      return *this;
+    }      
+
+    /**
+     * Destructor.
+     */
+    ~RandomGeneration();
+
+    /**
+     * Initialise the random generator.
+     *
+     * A uniform random number distribution is defined, which
+     * produces "real" values between 0 and 1 (0 inclusive, 1
+     * exclusive).
+     */
+    void init();
 
     // ////////// Attributes //////////
-    /** The seed of the random generator.
-        <br>The seed is unsigned, otherwise the wrong overload may be
-        selected when using mt19937 as the boost::base_generator_type. */
+    /**
+     * The seed of the random generator.
+     *
+     * The seed is unsigned, otherwise the wrong overload may be
+     * selected when using mt19937 as the boost::base_generator_type.
+     */
     RandomSeed_T _seed;
 
-    /** Random number generator engine.
-        <br>The random number generator is currently based on boost::minstd_rand.
-        Alternates are boost::mt19937, boost::ecuyer1988. */
+    /**
+     * Random number generator engine.
+     *
+     * The random number generator is currently based on boost::minstd_rand.
+     * Alternates are boost::mt19937, boost::ecuyer1988.
+     */
     BaseGenerator_T _generator;
-    
-    /** Random number generator.
-        <br>It is initialized with a reproducible seed and a uniform
-        distribution. */
-    boost::variate_generator<BaseGenerator_T&,
-                             boost::uniform_real<> > _uniformGenerator;
   };
 
 }
--- stdair/stdair_maths_types.hpp
+++ stdair/stdair_maths_types.hpp
@@ -10,38 +10,56 @@
 #include <map>
 // Boost Random
 #include <boost/random/linear_congruential.hpp>
-#include <boost/random/uniform_real.hpp>
+#include <boost/random/uniform_01.hpp>
 #include <boost/random/variate_generator.hpp>
 
 namespace stdair {
 
   // //////// Type definitions /////////
-  /** Define the replication number. */
+  /**
+   * Define the replication number.
+   */
   typedef unsigned int ReplicationNumber_T;
 
-  /** Define the seed type of an Exponential function. */
+  /**
+   * Define the seed type of an Exponential function.
+   */
   typedef unsigned long int ExponentialSeed_T;
 
-  /** Define the seed type of an Uniform function. */
+  /**
+   * Define the seed type of an Uniform function.
+   */
   typedef unsigned long int UniformSeed_T;
 
-  /** Seed for the random generation, so that it can be reproductible. */
+  /**
+   * Seed for the random generation, so that it can be reproductible.
+   */
   typedef unsigned long int RandomSeed_T;
   
-  /** Random number generator. */
+  /**
+   * Random number generator.
+   */
   typedef boost::minstd_rand BaseGenerator_T;
 
-  /** Uniform random generator. */
+  /**
+   * Uniform random generator.
+   */
   typedef boost::variate_generator<stdair::BaseGenerator_T&,
-                                   boost::uniform_real<> > UniformGenerator_T;
+                                   boost::uniform_01<> > UniformGenerator_T;
 
-  /** Define a mean value (e.g., 20.2). */
+  /**
+   * Define a mean value (e.g., 20.2).
+   */
   typedef double MeanValue_T;
 
-  /** Define a standard deviation value (e.g., 1.5). */
+  /**
+   * Define a standard deviation value (e.g., 1.5).
+   */
   typedef double StdDevValue_T;
 
-  /** Probability */
+  /**
+   * Probability.
+   */
   typedef float Probability_T;
 
 }
