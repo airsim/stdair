9874abb997f81996351d064f6f385c32781fb262
Anh Quan Nguyen <quan.nguyen@amadeus.com>
2011-03-23 15:15:25 +0100

    [test] Added the test for guillotine block.

--- stdair/bom/FareOptionStruct.hpp
+++ stdair/bom/FareOptionStruct.hpp
@@ -30,6 +30,11 @@ namespace stdair {
       return _fare;
     }
 
+    /** Get the availability. */
+    const Availability_T& getAvailability() const {
+      return _avl;
+    }
+    
     /** Get the change fees. */
     const ChangeFees_T getChangeFees() const {
       return _changeFee;
@@ -54,6 +59,11 @@ namespace stdair {
     /** Set the fare value. */
     void setFare (const Fare_T& iFare) {
       _fare = iFare;
+    }
+
+    /** Set the availability. */
+    void setAvailability (const Availability_T& iAvl) {
+      _avl = iAvl;
     }
 
     /** Set the change fees. */
@@ -130,6 +140,11 @@ namespace stdair {
      * Fare.
      */
     Fare_T _fare;
+
+    /**
+     * Avalability.
+     */
+    Availability_T _avl;
 
     /**
      * Change fees.
