fdd558541c68078d877248aac9254a094e9a827b
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-04-29 18:05:43 +0200

    [Config] The StdAir main library is now built in a dedicated sub-directory, namely 'core'.

--- stdair/ui/cmdline/sources.mk
+++ stdair/ui/cmdline/sources.mk
@@ -1,3 +1,3 @@
 #
-stdair_cmd_h_sources = $(top_srcdir)/stdair/ui/cmdline/SReadline.hpp
-stdair_cmd_cc_sources =
+stdair_uicl_h_sources = $(top_srcdir)/stdair/ui/cmdline/SReadline.hpp
+stdair_uicl_cc_sources =
