0689f72381f73f78a1016bc9b2050ae8b14edcab
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-06-08 01:56:09 +0200

    [Release] The database admin scripts are now well located (in /usr/libexec/stdair).

--- db/admin/sources.mk
+++ db/admin/sources.mk
@@ -1,3 +1,16 @@
+db_admin_sh_in_sources = \
+	$(top_srcdir)/db/admin/clean_all_tables.sh.in \
+	$(top_srcdir)/db/admin/clean_a_table.sh.in \
+	$(top_srcdir)/db/admin/count_all_tables.sh.in \
+	$(top_srcdir)/db/admin/count_a_table.sh.in \
+	$(top_srcdir)/db/admin/create_airinv_db.sh.in \
+	$(top_srcdir)/db/admin/create_airinv_user.sh.in \
+	$(top_srcdir)/db/admin/create_dsim_db.sh.in \
+	$(top_srcdir)/db/admin/create_dsim_user.sh.in \
+	$(top_srcdir)/db/admin/dump_airinv_db_structure.sh.in \
+	$(top_srcdir)/db/admin/dump_dsim_db_structure.sh.in \
+	$(top_srcdir)/db/admin/load_airinv_dsim_data.sh.in \
+	$(top_srcdir)/db/admin/load_dsim_data.sh.in
 db_admin_sh_sources = \
 	$(top_srcdir)/db/admin/clean_all_tables.sh \
 	$(top_srcdir)/db/admin/clean_a_table.sh \
@@ -11,10 +24,3 @@ db_admin_sh_sources = \
 	$(top_srcdir)/db/admin/dump_dsim_db_structure.sh \
 	$(top_srcdir)/db/admin/load_airinv_dsim_data.sh \
 	$(top_srcdir)/db/admin/load_dsim_data.sh
-db_admin_sql_sources = \
-	$(top_srcdir)/db/admin/create_airinv_tables.sql \
-	$(top_srcdir)/db/admin/create_airinv_user.sql \
-	$(top_srcdir)/db/admin/create_dsim_tables.sql \
-	$(top_srcdir)/db/admin/create_dsim_user.sql \
-	$(top_srcdir)/db/admin/fill_airinv_tables.sql \
-	$(top_srcdir)/db/admin/fill_dsim_tables.sql
