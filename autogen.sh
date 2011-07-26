8732d42eb898776fca08cd246dd5e3c6b5745568
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-07-26 19:09:45 +0200

    [Build] The fake (CMake-based) Autotools autogen.sh/configure scripts are working fine.

--- autogen.sh
+++ autogen.sh
@@ -11,13 +11,30 @@ then
     exit 1
 fi
 
-#
-ARG_DOC=${1}
-DOC_OPTION=""
-if [ "${ARG_DOC}" = "off" -o "${ARG_DOC}" = "OFF" ]
-then
-    DOC_OPTION="-DINSTALL_DOC:BOOL=OFF"
-fi
+##
+# Parsing of command-line options
+DOC_OPTION="-DINSTALL_DOC:BOOL=ON"
+for opt_elem in $@
+do
+  if [ "${opt_elem}" = "-h" -o "${opt_elem}" = "-H" -o "${opt_elem}" = "--h" -o "${opt_elem}" = "--help" ]
+  then
+    echo ""
+    echo " This script generates a 'configure' script for simulator-related projects."
+    echo " That 'configure' script is compatible with some GNU Autotools options."
+    echo " Run './configure', after its generation, to see its usage."
+    echo " "
+    echo "Usage:"
+    echo "    \$0 [--nodoc]"
+    echo ""
+    exit 0
+  fi
+  if [ "${opt_elem}" = "off" -o "${opt_elem}" = "OFF" -o "${opt_elem}" = "--nodoc" ]
+  then
+      DOC_OPTION="-DINSTALL_DOC:BOOL=OFF"
+  fi
+done
+
+#cmake -DCMAKE_INSTALL_PREFIX=/home/user/dev/deliveries/stdair-0.35.0 -DLIB_SUFFIX=64 -DCMAKE_BUILD_TYPE:STRING=Debug
 
 #
 VERSION_MAJOR=0
@@ -42,6 +59,62 @@ then
 fi
 
 #
+cat > configure << _EOF
+#!/bin/sh
+
+##
+# Parsing of options
+# Note: the build server:
+#  * builds StdAir with the following option:
+#    ./configure --prefix=/opt/stdair-0.36.0
+#  * builds other simulator-related projects with the following option:
+#    ./configure --with-stdair=/opt/stdair
+#
+PREFIX_OPTION=""
+STDAIR_OPTION=""
+DOC_OPTION="${DOC_OPTION}"
+RM_OPTION="rm -rf build"
+for opt_elem in \$@
+do
+  if [ "\${opt_elem}" = "-h" -o "\${opt_elem}" = "-H" -o "\${opt_elem}" = "--h" -o "\${opt_elem}" = "--help" ]
+  then
+    echo ""
+    echo "Usage:"
+    echo "    \$0 [--prefix=<install_dir> | --with-stdair=<stdair_install_dir>] [--with-doc | --without-doc] [-n|-N|--norm]"
+    echo "      --with-doc/--without-doc : Force the (resp. non) generation of the documentation" 
+    echo "      -n/-N/--norm             : Do not remove/clean older potential 'build' sub-directory" 
+    echo ""
+    exit 0
+  fi
+  IS_OPTION_PREFIX=\`echo "\${opt_elem}" | grep "^--prefix="\`
+  if [ "\${IS_OPTION_PREFIX}" != "" ]
+  then
+    PREFIX_DIR=\`echo "\${opt_elem}" | sed -e "s/^--prefix=\(.*\)\$/\1/"\`
+    PREFIX_OPTION="-DCMAKE_INSTALL_PREFIX=\${PREFIX_DIR}"
+  fi
+  IS_OPTION_STDAIR=\`echo "\${opt_elem}" | grep "^--with-stdair="\`
+  if [ "\${IS_OPTION_STDAIR}" != "" ]
+  then
+    STDAIR_DIR=\`echo "\${opt_elem}" | sed -e "s/^--with-stdair=\(.*\)\$/\1/"\`
+    STDAIR_OPTION="-DWITH_STDAIR_PREFIX=\${STDAIR_DIR}"
+  fi
+  IS_OPTION_W_DOC=\`echo "\${opt_elem}" | grep "^--with-doc"\`
+  if [ "\${IS_OPTION_W_DOC}" != "" ]
+  then
+    DOC_OPTION="-DINSTALL_DOC:BOOL=ON"
+  fi
+  IS_OPTION_WO_DOC=\`echo "\${opt_elem}" | grep "^--without-doc"\`
+  if [ "\${IS_OPTION_WO_DOC}" != "" ]
+  then
+    DOC_OPTION="-DINSTALL_DOC:BOOL=OFF"
+  fi
+  if [ "\${opt_elem}" = "-n" -o "\${opt_elem}" = "-N" -o "\${opt_elem}" = "--norm" ]
+  then
+    RM_OPTION=""
+  fi
+done
+
+#
 LIB_OPTION=""
 if [ -d /usr/lib64 ]
 then
@@ -49,31 +122,38 @@ then
 fi
 
 #
-CMAKE_CMD="cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ${LIB_OPTION} -DCMAKE_BUILD_TYPE:STRING=Debug ${DOC_OPTION} .."
+BUILD_OPTION="-DCMAKE_BUILD_TYPE:STRING=Debug"
 
 #
-cat > configure << _EOF
-#!/bin/sh -x
+CMAKE_CMD="cmake \${PREFIX_OPTION} \${STDAIR_OPTION} \${LIB_OPTION} \${BUILD_OPTION} \${DOC_OPTION} .."
 
+# Trace on
+set -x
+
+##
 # Clean potential former builds
-rm -rf build
-mkdir build
+\${RM_OPTION}
+mkdir -p build
 
+##
 # Configuration
 cd build
-${CMAKE_CMD}
+\${CMAKE_CMD}
 cd -
 
 # Trace back off
 set +x
 
+##
 # Tell how to build, test and install
 echo ""
 echo "--------------------"
 echo "To build, test and install:"
-echo "cd build && make && make check && make install && cd -"
+echo "alias build_check_install='cd build && make && make check && make install && cd -'"
+echo "build_check_install"
 echo "--------------------"
 echo ""
+
 _EOF
 
 #
