f60814bfe502d6ae19093c8ac04f0c6771229c55
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-08-29 23:42:31 +0200

    [Misc] Suppressed generated files.

--- samples/rds01/rds01.bk2/sources.mk
+++
@@ -1,5 +0,0 @@
-sample_rds01_csv_sources = \
-	$(top_srcdir)/samples/rds01/demand.csv \
-	$(top_srcdir)/samples/rds01/schedule.csv \
-	$(top_srcdir)/samples/rds01/fare.csv \
-	$(top_srcdir)/samples/rds01/yield.csv
--- test/stdair/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/localoriuser/dev/sim/stdair/stdairgit/test/stdair
-# Build directory: /home/localoriuser/dev/sim/stdair/stdairgit/test/stdair
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(StdAirTesttst "StdAirTest")
