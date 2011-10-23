bfc1c67d90ca23d94b0956746cb4f6d874f40b7d
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-10-23 17:22:34 +0200

    [Release 0.4.0] Release of the 0.4.0 version of StdAir.

---
+++ stdair/sources.mk
@@ -0,0 +1,8 @@
+stdair_service_h_sources = $(top_srcdir)/stdair/STDAIR_Types.hpp \
+					$(top_srcdir)/stdair/STDAIR_Service.hpp
+stdair_service_cc_sources =
+
+stdair_service_h_sources = $(top_srcdir)/stdair/STDAIR_Types.hpp \
+					$(top_srcdir)/stdair/STDAIR_Service.hpp
+stdair_service_cc_sources =
+
