1dc0eb39ec5e9e95bcb22092dea5f82e792adc1d
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-10-23 17:29:19 +0200

    [Release 0.6.0] Release of the 0.6.0 version of StdAir.

--- stdair.spec
+++ stdair.spec
@@ -2,7 +2,7 @@
 %global mydocs __tmp_docdir
 #
 Name:           stdair
-Version:        0.5.0
+Version:        0.6.0
 Release:        1%{?dist}
 
 Summary:        C++ Standard Airline IT Object Library
@@ -111,7 +111,13 @@ rm -rf $RPM_BUILD_ROOT
 
 
 %changelog
-* Sun Aug 30 2010 Son Nguyen Kim <nguyenkims@gmail.com> 0.5.0-1
+* Thu Oct 21 2010 Son Nguyen Kim <nguyenkims@gmail.com> 99.99.99-1
+- Upstream update
+
+* Thu Oct 14 2010 Son Nguyen Kim <nguyenkims@gmail.com> 0.6.0-1
+- Upstream update
+
+* Thu Sep 16 2010 Son Nguyen Kim <nguyenkims@gmail.com> 0.5.0-1
 - Upstream update
 
 * Fri Sep 03 2010 Son Nguyen Kim <nguyenkims@gmail.com> 0.4.0-1
--- stdair/bom/FareFamily.hpp
+++ stdair/bom/FareFamily.hpp
@@ -1,29 +1,38 @@
-#ifndef __STDAIR_BOM_BOMROOT_HPP
-#define __STDAIR_BOM_BOMROOT_HPP
+#ifndef __STDAIR_BOM_FAREFAMILY_HPP
+#define __STDAIR_BOM_FAREFAMILY_HPP
 
 // //////////////////////////////////////////////////////////////////////
 // Import section
 // //////////////////////////////////////////////////////////////////////
-// Boost Random
-#include <boost/random/uniform_real.hpp>
-#include <boost/random/variate_generator.hpp>
-// STDAIR
+// STDAIR 
 #include <stdair/bom/BomAbstract.hpp>
-#include <stdair/bom/BomRootKey.hpp>
+#include <stdair/bom/FareFamilyKey.hpp>
+#include <stdair/bom/FareFamilyTypes.hpp>
 
 namespace stdair {
-  /** Class representing the actual attributes for the Bom root. */
-  class BomRoot : public BomAbstract {
+
+  /** Class representing the actual attributes for a family fare. */
+  class FareFamily : public BomAbstract {
     template <typename BOM> friend class FacBom;
     friend class FacBomManager;
-    
-  public:
-    /** Definition allowing to retrieve the associated BOM key type. */
-    typedef BomRootKey Key_T;
 
   public:
-    // /////////// Getters //////////////
+    // Type definitions.
+    /** Definition allowing to retrieve the associated BOM key type. */
+    typedef FareFamilyKey Key_T;
+
+  public:
+    // /////////// Getters ////////////
+    /** Get the family fare key. */
     const Key_T& getKey() const { return _key; }
+    
+    /** Get the family fare code (part of the primary key). */
+    const FamilyCode_T& getFamilyCode() const { return _key.getFamilyCode(); }
+
+    /** Get the parent object. */
+    BomAbstract* const getParent() const { return _parent; }
+
+    /** Get the map of children holders. */
     const HolderMap_T& getHolderMap() const { return _holderMap; }
 
   public:
@@ -36,33 +45,27 @@ namespace stdair {
         @param istream& the input stream. */
     void fromStream (std::istream& ioIn) { }
 
-   /** Get the serialised version of the Business Object. */
+    /** Get the serialised version of the Business Object. */
     std::string toString() const;
     
-    /** Get a string describing the  key. */
+    /** Get a string describing the key. */
     const std::string describeKey() const { return _key.toString(); }
-    
+
   protected:
     /** Default constructors. */
-    BomRoot ();
-    BomRoot (const BomRoot&);
-    BomRoot (const Key_T& iKey);
+    FareFamily (const Key_T&);
+    FareFamily (const FareFamily&);
     /** Destructor. */
-    ~BomRoot();
+    ~FareFamily();
 
-  protected:
+  public:
     // Attributes
     Key_T _key;
+    BomAbstract* _parent;
     HolderMap_T _holderMap;
-
-    // TEST
-    stdair::RandomSeed_T _seed;
-    stdair::BaseGenerator_T _generator;
-    boost::variate_generator<stdair::BaseGenerator_T&,
-                             boost::uniform_real<> > _uniformGenerator;
-  public:
-    RandomSeed_T generateSeed ();
+    
   };
 
 }
-#endif // __STDAIR_BOM_BOMROOT_HPP
+#endif // __STDAIR_BOM_FAREFAMILY_HPP
+
