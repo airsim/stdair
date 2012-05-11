e0b05c8deee2cbc9d9207a3e85371ab7d816eeef
Adelving Robin <adelving@A457-VirtualBox.(none)>
2012-05-11 16:27:54 +0200

    [DEV] Added the Policy Structure and the Revenue typedef.

---
+++ stdair/bom/PolicyTypes.hpp
@@ -0,0 +1,24 @@
+// //////////////////////////////////////////////////////////////////////
+#ifndef __STDAIR_BOM_POLICYTYPES_HPP
+#define __STDAIR_BOM_POLICYTYPES_HPP
+
+// //////////////////////////////////////////////////////////////////////
+// Import section
+// //////////////////////////////////////////////////////////////////////
+// STL
+
+#include <list>
+
+
+
+namespace stdair {
+
+  // Forward declarations.
+  struct PolicyStruct;
+
+  /** Define a list of policies */
+  typedef std::list<PolicyStruct> PolicyList_T;
+
+     
+}
+#endif // __STDAIR_BOM_POLICYTYPES_HPP
