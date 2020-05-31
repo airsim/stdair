b623f909e1637671cad6513d673e2ac01966bdc7
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2020-05-31 04:44:57 +0200

    [Config] Improved guard around new CMP policies

--- ci-scripts/soci-install-debian.sh
+++ ci-scripts/soci-install-debian.sh
@@ -5,7 +5,7 @@ pushd /opt/soci
 patch -p1 < ./soci-debian-cmake.patch
 mkdir -p build
 pushd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DSOCI_CXX_C11=ON -DSOCI_TESTS=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DSOCI_CXX11=ON -DSOCI_TESTS=OFF ..
 sudo make install
 popd
 popd
